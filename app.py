608ee9780feecedcc5580b5cbcda3cb4df2ee154
GooRooCred <bruno.mascio@gooroocredito.com.br>
2026-05-15 15:26:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -56,12 +56,14 @@ COLUNAS_AMIGAVEIS = {
 # FUNÇÃO DATA BR
 #================================
 def format_data_br(valor):
-            return ""
+
     try:
-        
+
         if pd.isna(valor):
-           
+            return ""
+
         return pd.to_datetime(valor).strftime("%d/%m/%Y")
+
     except:
         return valor
 
